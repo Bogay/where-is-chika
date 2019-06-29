d39fb890f10419ecaafe76e5cad2510e0fd40e3f
Bogay <pojay11523@gmail.com>
2019-06-30 00:23:24 +0800

    add comments and error handling

--- pixiv_simple_crawl.py
+++ pixiv_simple_crawl.py
@@ -9,12 +9,21 @@ from bs4 import BeautifulSoup as bs
 def query_page(key_word, page_idx):
     url = f'https://www.pixiv.net/search.php?s_mode=s_tag&word={key_word}&p={page_idx}'
 
+    # search for IDs
     resp = rq.get(url)
     soup = bs(resp.text, 'lxml')
+    # look for a <input> tag whose id is 'js-mount-point-search-result-list'
     tag = soup.find('input', id='js-mount-point-search-result-list')
 
+    # if not found
+    if not tag:
+        print(f'can not find ids in {key_word}, p.{page_idx}')
+
+    # get the 'data-items' attribute and parse it
     items = json.loads(tag['data-items'])
+    # get IDs
     ill_ids = [item['illustId'] for item in items]
+    # filter None
     ill_ids = [_id for _id in ill_ids if _id]
 
     return ill_ids
@@ -23,12 +32,13 @@ def query_keyword(key_word):
     idx = 1
     ret = set()
 
-    while True:
-        temp = set(query_page(key_word, idx))
-        if len(temp - ret) == 0: break
+    # because guest can browse only 10 pages
+    while idx <= 10:
+        temp = query_page(key_word, idx)
+        if len(temp) == 0: break
 
-        print('crawl id: %s - p.%d' % (key_word, idx))
-        ret |= temp
+        print(f'get id: {key_word} - p.{idx}')
+        ret |= set(temp)
         idx += 1
 
     return ret
@@ -38,25 +48,32 @@ def crawl_illust(base_dir, illust_id):
     resp = rq.get(target)
     soup = bs(resp.text, 'lxml')
 
-    # get image src
+    # get image
     target_img = soup.find(lambda x: x.name == 'img' and x['src'].find('600x600') != -1)
 
+    # check whether we find the image
     if target_img:
+        # get the image source path
         target_img = target_img['src']
+        # use illust id as filename, avoid special characters
         img_title = illust_id + '.jpg'
 
+        # if the file exists, skip it to save time
         if os.path.isfile(img_title):
             print(f'{img_title} exist, skip.')
             return
 
         print(f'download {img_title}')
 
+        # download the image, ensure you set the Referer
         resp = rq.get(target_img, headers = { 'Referer': target })
         if resp.status_code == 200:
             with open(base_dir + img_title, 'wb+') as f:
                 f.writelines(resp)
-        else:
+        else: # donload fail
             print(f'download {illust_id} fail!')
+    else: # can not find the image by id
+        print(f'can not found {illust_id}')
         
 if __name__ == '__main__':
     ill_ids = set()
